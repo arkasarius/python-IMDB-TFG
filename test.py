c33f130ced57a3133857c52d95999f585e0b64a6
Roger Ordoñez Brumos <roger.ordonez.brumos@gmail.com>
2019-03-27 18:01:21 +0100

    Download images from "matrix" 1 for each actor

--- test.py
+++ test.py
@@ -2,6 +2,12 @@ from imdb import IMDb
 # https://github.com/alberanid/imdbpy/blob/master/docs/usage/query.rst
 # requires  [console] pip install imdbpy
 
+# https://github.com/hardikvasa/google-images-download#examples
+# requires [console] pip install google_images_download
+from google_images_download import google_images_download   
+response = google_images_download.googleimagesdownload() 
+
+
 ia = IMDb()
 data=ia.search_movie('The Matrix')
 if data:
@@ -10,4 +16,6 @@ if data:
         if movie:
             cast=movie.get('cast')
             for actor in cast:  
-                print (actor['name'])
+                arguments = {"keywords": actor['name'],"limit":1,"print_urls":True}
+                paths = response.download(arguments)   
+                print(paths)  
