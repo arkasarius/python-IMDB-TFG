9ffa93d1847e2ecdb1a3c41155811577133616d5
Roger Ordoñez Brumos <roger.ordonez.brumos@gmail.com>
2019-03-27 17:14:46 +0100

    requirements

--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 from imdb import IMDb
-
+# https://github.com/alberanid/imdbpy/blob/master/docs/usage/query.rst
+# requires  [console] pip install imdbpy
 
 ia = IMDb()
 data=ia.search_movie('The Matrix')
